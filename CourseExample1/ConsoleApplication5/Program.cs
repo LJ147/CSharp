3f6306decd29f195768d6e17d0f153f1e7f9254f
LJ <1197003451@qq.com>
2016-09-26 07:00:01 +0800

    add some demoes

---
+++ CourseExample1/ConsoleApplication5/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+ *第一次C#课程作业示例代码
+ *
+ *作者：贾向阳
+ *
+ * */
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ISS.CSharp.Example
+{
+    /// <summary>
+    ///  
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program24();
+            //program26();
+            //program27();
+            program28();
+        }
+
+        /// <summary>
+        /// 24题示例
+        /// </summary>
+        static void program24()
+        {
+            int num = int.Parse(Console.ReadLine());
+            string str = (num % 2 == 0) ? "偶数" : "奇数";
+            Console.WriteLine("{0}是" + str + "!", num);
+        }
+
+
+        /// <summary>
+        /// 26题示例
+        /// </summary>
+        static void program26()
+        {
+            int radius = int.Parse(Console.ReadLine());
+            Console.WriteLine("diameter={0}", radius * 2);
+            Console.WriteLine("circumference={0:f3}", radius * 2 * Math.PI);
+            Console.WriteLine("area={0:f3}", radius * radius * Math.PI);
+        }
+
+        /// <summary>
+        /// 27题示例
+        /// </summary>
+        static void program27()
+        {
+            char[] chars = { 'A', 'B', 'C', 'a', 'b', 'c', '0', '1', '2', '$', '*', '+', '/', ' ' };
+            foreach (char c in chars)
+            {
+                Console.WriteLine("{0}={1}", c, (int)c);
+            }
+        }
+
+        /// <summary>
+        /// 28题示例
+        /// 采用循环除10的方法摘取每一位数字
+        /// </summary>
+        static void program28()
+        {
+            int num = int.Parse(Console.ReadLine());
+            ArrayList numList = new ArrayList(); //分解后的数字
+            for (int n = num; n > 0; n = n / 10)
+            {
+                numList.Add(n % 10);
+            }
+            Console.WriteLine("{0}分解后数字为:", num);
+            for (int i = numList.Count - 1; i >= 0; i--)
+            {
+                Console.Write("{0}   ", numList[i]);
+            }
+        }
+    }
+
+
+}
---
+++ HomeWork02/HomeWork02/CodeExample2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example8_17();
+            Example8_19();
+        }
+        
+
+        public static void Example8_17()
+        {
+            const int N = 3600;
+            Random random = new Random();
+            int[] points= new int[11];
+            for (int i = 0; i < N; i++)
+            {
+                points[random.Next(1, 7) + random.Next(1, 7) - 2]++;
+            }
+            for (int i = 0; i < points.Length; i++)
+            {
+                System.Console.WriteLine("{0}点概率：{1:f4}", i + 2, (float)points[i] / N);
+            }
+        }
+
+     
+
+        private static void Example8_19()
+        {
+            bool[] seats = new bool[10];//defaul values are all False
+            int nextFirstClassSeat = 0;
+            int nextEconomySeat = seats.Length / 2;
+
+            while (true)
+            {
+                Console.Write("Please type 1 for First Class, please type 2 for Economy:");
+                int seatType = int.Parse(Console.ReadLine());
+                if (seatType != 1 && seatType != 2)
+                {
+                    Console.WriteLine("Invalid input！");
+                    continue;
+                }
+
+                //check avaliability
+                if (nextFirstClassSeat > 4 && nextEconomySeat > 9)
+                {
+                    Console.WriteLine("No seat is avaliable！");
+                    return;
+                }
+
+                //change seat type?
+                string change = null;
+                if (seatType == 1 && nextFirstClassSeat > 4 && nextEconomySeat <= 9)
+                {
+                    Console.WriteLine("No first class seats is avaliable，would you like to change to economy seats？(Y/N)");
+                    change = Console.ReadLine();
+                }
+                else if (seatType == 2 && nextEconomySeat > 9 && nextFirstClassSeat <= 4)
+                {
+                    Console.WriteLine("No economy seats is avaliable，would you like to change to first class seats？(Y/N)");
+                    change = Console.ReadLine();
+                }
+
+                if ("Y".Equals(change) || "y".Equals(change))
+                {
+                    seatType = (seatType == 1) ? 2 : 1;
+                }
+                else if ("N".Equals(change) || "n".Equals(change))
+                {
+                    Console.WriteLine("Next flight leaves in 3 hours!");
+                    continue;
+                }
+                else if (change != null)
+                {
+                    Console.WriteLine("Invalid input！");
+                    continue;
+                }
+
+                // arrange seat
+                if (seatType == 1)
+                {
+                    Console.WriteLine("arrange to seat {0}:", nextFirstClassSeat);
+                    seats[nextFirstClassSeat++] = true;
+                }
+                else
+                {
+                    Console.WriteLine("arrange to seat {0}:" , nextEconomySeat);
+                    seats[nextEconomySeat++] = true;
+                }
+            }
+        }
+
+    }
+}
